ffedc51b498c35bb1974d5e536b3c10f400f8a73
JaoVicy <filipimjoa@gmail.com>
2026-06-10 19:32:03 -0300

    perf: adding __init__ file to simplify imports

---
+++ python/utils/__init__.py
@@ -0,0 +1,5 @@
+from .cleaner import tratar_dados
+from .db import salvar_no_supabase
+from .simulation import criar_tarefa_clickup_simulada
+
+__all__ = ["tratar_dados", "salvar_no_supabase", "criar_tarefa_clickup_simulada"]
